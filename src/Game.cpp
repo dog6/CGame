66da053e21c5f18c42fccb379e2a2e9a159ac42b
Smallz.dev <benjaminsmallarz@gmail.com>
2025-03-23 21:14:00 -0400

    Added pulsing circle and comments for how draw order currently works

--- src/Game.cpp
+++ src/Game.cpp
@@ -59,13 +59,20 @@ void Game::Start(){
     SetTargetFPS(60);
 
     // Create entities
+    
+    // Create tilemap
     float tileSize = 50;
     float tileGap = 1;
     Tilemap* map = new Tilemap(Vector2Zero(), Vector2{screenWidth/(tileSize+tileGap),screenHeight/(tileSize+tileGap)}, Vector2{tileSize,tileSize}, tileGap); 
-
     tilemap.shape = map; // give tilemap it's shape
+
+    
+    // Create ellipse
+    ball.shape = new Ellipse(12, BLACK); // give ball shape
+    // ball.shape = new Ellipse(Vector2{36, 12}, BLUE); // give ball shape
+    
+    // Create rectangle
     cursorRectangle.shape = new Rect(Vector2{12, 12}, RED); // give cursor shape
-    ball.shape = new Ellipse(12, BLUE); // give ball shape
     
     /*
     * Add entites to scene
@@ -77,10 +84,11 @@ void Game::Start(){
     */
 
     // Or add multiple entities at once instead
-    demoScene.AddEntities(vector<Entity>{tilemap, cursorRectangle, ball});
+    demoScene.AddEntities(vector<Entity>{tilemap, ball, cursorRectangle});
 
 }
 
+bool ball_growing = true;
 
 /* Update loop */
 void Game::Update(){
@@ -92,6 +100,21 @@ void Game::Update(){
     Vector2 mousePos = GetMousePosition();
     Vector2 sz = cursorRectangle.shape->getSize(); 
     cursorRectangle.body->position = Vector2{mousePos.x-(sz.x/2),mousePos.y-(sz.y/2)};
+
+    // Increase size of ball (ideally this logic won't live here)
+    if (ball_growing){
+        ball.shape->setSize(Vector2AddValue(ball.shape->getSize(), 1)); // grow
+        
+        if (ball.shape->getSize().x > 100){
+            ball_growing = false; // Check if max size
+        }
+
+    } else {
+        ball.shape->setSize(Vector2SubtractValue(ball.shape->getSize(), 1)); // shrink
+        if (ball.shape->getSize().x <= 10){
+            ball_growing = true; // Check if min size
+        }
+    }
 
 }
 
